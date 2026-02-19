9841020b85002ff080da6250684030ffa7a12248
suvssagrawal <suvssextras@gmail.com>
2026-02-19 13:37:43 +0530

    demo check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './components/Home'
-import Movies from './components/Movies'
-import MovieDetails from './components/MovieDetails'
-import SeatLayout from './components/SeatLayout'
-import MyBookings from './components/MyBookings'
-import Favorite from './components/Favorite'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favorite from './pages/Favorite'
 import {Toaster} from 'react-hot-toast'
+import Footer from './components/Footer'
 const App = () => {
   const isAdminRoute = useLocation().pathname.startsWith('/admin');
    return (
@@ -22,6 +23,7 @@ const App = () => {
       <Route path='/my-bookings' element={<MyBookings/>}/>
       <Route path='/favorite' element={<Favorite/>}/>
     </Routes>
+    {isAdminRoute ? null : <Footer />}
     </>
   )
 }
