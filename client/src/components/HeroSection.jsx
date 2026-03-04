fcadaeb72b75f31f86a94e2bd8758c35becaf8c8
suvssagrawal <suvssextras@gmail.com>
2026-03-05 00:18:40 +0530

    added the feature section page

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
   const navigate = useNavigate()
 
   return (
-        <div className=' flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("backgroundImage.png")] bg-cover bg-center h-screen '>
+        <div className=' flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] bg-cover bg-center h-screen '>
           <img src={marvelLogo} alt=""  className='max-h-11 lg:h-11 mt-20'/>
           <h1>Guardians  <br /> of the Galaxy</h1>
           <div className='flex items-center gap-4 text-gray-300'>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import FeatureSection from '../components/FeatureSection'
 
 const Home = () => {
   return (
     <>
     <HeroSection />
+    <FeatureSection />
     </>
   )
 }
